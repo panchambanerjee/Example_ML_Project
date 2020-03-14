8d4585bdf41d99611c406da482c0d46291c69cf8
delayedkarma <panchajanya.banerjee@gmail.com>
2020-03-14 14:14:45 -0700

    Updated inference code

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 export TRAINING_DATA=input/train_folds.csv
+export TEST_DATA=input/test.csv
 
-FOLD=0 python -m src.train
+export MODEL=$1
+
+# FOLD=0 python -m src.train
+# FOLD=1 python -m src.train
+# FOLD=2 python -m src.train
+# FOLD=3 python -m src.train
+# FOLD=4 python -m src.train
+
+python -m src.predict
